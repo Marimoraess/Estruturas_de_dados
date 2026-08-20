14c09f50007118104663f9f1036a56aac9e9e9f4
marym0raes <moraesmariana417@gmail.com>
2026-08-20 01:44:34 -0300

    ex

--- Exercícios/simetrica_arv.c
+++ Exercícios/simetrica_arv.c
@@ -7,44 +7,86 @@ typedef struct no {
     struct no *dir;
 } TNo;
 
+typedef struct pilha {
+    TNo *info;
+    struct pilha *prox;
+} TPilha;
+
 TNo* criaNo(int valor) {
     TNo *novo = (TNo*) malloc(sizeof(TNo));
+
     novo->info = valor;
     novo->esq = NULL;
     novo->dir = NULL;
+
     return novo;
 }
 
-void ordem(TNo *raiz) {
-    TNo *p = raiz;
-    TNo *pilha[100];
-    int topo = -1;
+void push(TPilha **p, TNo *no) {
+    TPilha *novo = (TPilha*) malloc(sizeof(TPilha));
 
-    while (p != NULL || topo != -1) {
+    novo->info = no;
+    novo->prox = *p;
+    *p = novo;
+}
 
-        while (p != NULL) {
-            pilha[++topo] = p;
-            p = p->esq;
+TNo* pop(TPilha **p) {
+    TPilha *aux;
+    TNo *no;
+
+    if (*p == NULL)
+        return NULL;
+
+    aux = *p;
+    no = aux->info;
+    *p = aux->prox;
+
+    free(aux);
+
+    return no;
+}
+
+int pilhaVazia(TPilha *p) {
+    return p == NULL;
+}
+
+void emOrdem(TNo *raiz) {
+    TPilha *p = NULL;
+    TNo *atual = raiz;
+
+    while (atual != NULL || !pilhaVazia(p)) {
+
+        while (atual != NULL) {
+            push(&p, atual);
+            atual = atual->esq;
         }
 
-        p = pilha[topo--];
+        atual = pop(&p);
 
-        printf("%d ", p->info);
+        printf("%d ", atual->info);
 
-        p = p->dir;
+        atual = atual->dir;
     }
 }
 
 int main() {
 
-    TNo *raiz = criaNo(24);
-    raiz->esq = criaNo(59);
-    raiz->dir = criaNo(5);
-    raiz->esq->esq = criaNo(29);
-    raiz->esq->dir = criaNo(74);
+    TNo *raiz = criaNo(25);
 
-    printf("Caminho: ");
-    ordem(raiz);
+    raiz->esq = criaNo(13);
+    raiz->dir = criaNo(30);
+
+    raiz->esq->esq = criaNo(8);
+    raiz->esq->dir = criaNo(17);
+
+    raiz->dir->esq = criaNo(27);
+    raiz->dir->dir = criaNo(35);
+
+    printf("Percurso em ordem simetrica: ");
+
+    emOrdem(raiz);
+
+    printf("\n");
 
     return 0;
 }
